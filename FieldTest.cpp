962da9634a82be3874a8e23c0c25fee93b13438d
pagrawal1407 <pagrawal@mail.csuchico.edu>
2016-03-07 11:13:57 -0800

    commit

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,42 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+TEST(FieldTest, placeMineInBounds)
+{
+	Field minefield;
+	
+	minefield.placeMine(4,5);
+	ASSERT_FALSE( minefield.isSafe(4,5) );
+}
+TEST(FieldTest, placeMineInBounds)
+{
+	Field minefield;
+	
+	minefield.placeMine(4,5);
+	ASSERT_FALSE( minefield.isSafe(4,5) );
+	//ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+TEST(FieldTest, placeMineInBounds)
+{
+	Field minefield;
+	
+	minefield.placeMine(4,5);
+	ASSERT_TRUE( minefield.isSafe(3,5) );
+	//ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+TEST(FieldTest, placeMineInBounds)
+{
+	Field minefield;
+	
+	minefield.placeMine(4,5);
+	ASSERT_FALSE( minefield.isSafe(4,8) );
+	//ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+TEST(FieldTest, placeMineInBounds)
+{
+	Field minefield;
+	
+	minefield.placeMine(4,5);
+	ASSERT_FALSE( minefield.isSafe(14,5) );
+	//ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
